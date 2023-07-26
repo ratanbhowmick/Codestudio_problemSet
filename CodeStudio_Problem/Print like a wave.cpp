16406eb4d883686eba0629602847754d49510c62
Ratan Bhowmick <106478004+ratanbhowmick@users.noreply.github.com>
2023-07-26 17:31:24 +0530

    Add files via upload

---
+++ CodeStudio_Problem/Print like a wave.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    //Write your code here
+    vector<int> ans;
+
+    for(int col=0;col<mCols; col++){
+        
+        if(col&1){
+
+            // odd index -> Bottam to Top   
+            for(int row =nRows-1; row>=0; row--){
+                ans.push_back(arr[row][col]);
+            }
+
+        }
+        else{
+            // even or 0 index -> Top to Bottam
+
+            for(int row =0; row<nRows; row++){
+                ans.push_back(arr[row][col]);
+            }
+        }
+
+
+    }
+    return ans;
+
+}
